abdc2317a1d3da1fb7fb982fe0cbf902c940ab91
Sergi Kotrikadze <sergikotrikadze1@gmail.com>
2024-07-18 04:09:27 +0400

    Lang syntax done all tests pass

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,18 @@
 package com.epam.langSyntax;
 
+
 public class NumberReverter {
 
     public void revert(int number) {
+        int result = 0;
+
+        while (number != 0) {
+            result = result * 10 + number % 10;
+            number /= 10;
+        }
+
+        System.out.println(result);
+
     }
 
 }
